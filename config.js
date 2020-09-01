15933eb9698688afb7b6fb0b2f009d4caed7fda1
Brandon Gart <brandongart@gmail.com>
2020-09-01 14:03:36 -0600

    started deployment

--- config.js
+++ config.js
@@ -1,6 +1,22 @@
-module.exports = {
-    port: 3000,
+const fs = require('fs');
+const DEV = fs.existsSync(".dev");
+const config = {
+    port: !DEV ? process.env.PORT : 3000,
     db: {
         url: "localhost:27017/workoutTracker",
     },
 };
+
+
+const REMOTE = "{{user}}:{{password}}@{{host}}/{{dbname}}?retryWrites=true&w=majority"
+if (!DEV){
+	config.db = {
+		user: process.env.ATDB_USERNAME,
+		password: process.env.ATDB_PASSWORD,
+		host: process.env.ATDB_HOST,
+		dbname: process.env.ATDB_DBNAME,
+	}
+	// config.db.url = REMOTE  start here
+}
+
+module.exports = config
